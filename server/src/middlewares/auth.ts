6f5e61ae7f7b0b6bf981e25a933e7fb9a8cd7827
wojciechrut <wojciechrut1998@gmail.com>
2022-10-17 11:26:38 +0200

    Remove ./idea from repo

--- server/src/middlewares/auth.ts
+++ server/src/middlewares/auth.ts
@@ -1,31 +1,32 @@
-import { MeResponseLocals, ErrorStatus } from '../@types';
-import { createError } from '../utils/helpers/create-error';
-import { RequestHandler } from 'express';
-import UserRepository from '../repositories/user';
-import JWT from './../utils/helpers/jtw';
-import { _id } from '../utils/helpers/mongo';
+import { MeResponseLocals, ErrorStatus } from "../@types";
+import { createError } from "../utils/helpers/create-error";
+import { RequestHandler } from "express";
+import UserRepository from "../repositories/user";
+import JWT from "./../utils/helpers/jtw";
+import { _id } from "../utils/helpers/mongo";
 
 const auth: RequestHandler<any, any, any, any, MeResponseLocals> = async (
   request,
   response,
   next
 ) => {
-  const token = JWT.extractFromHeader(request.header('Authorization'));
+  const token = JWT.extractFromHeader(request.header("Authorization"));
   const { withFriends } = request.query;
 
   if (!token) {
-    next(createError(ErrorStatus.UNAUTHORIZED, 'Authorization failed'));
+    next(createError(ErrorStatus.UNAUTHORIZED, "Authorization failed"));
     return;
   }
 
   const decodedId = JWT.decode(token);
   const user = await UserRepository.findOne(
     { _id: _id(decodedId) },
-    withFriends === 'true'
+    withFriends === "true"
   );
 
   if (!user) {
-    next(createError(ErrorStatus.UNAUTHORIZED, 'Invalid token.'));
+    next(createError(ErrorStatus.UNAUTHORIZED, "Invalid token."));
+    return;
   } else {
     response.locals = { ...user.toObject(), token };
     next();
