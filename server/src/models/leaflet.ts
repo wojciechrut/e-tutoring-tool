36629acb549a530fbf0e0d2e68b54d8bf56871c3
wojciechrut <wojciechrut1998@gmail.com>
2022-10-19 21:26:39 +0200

    Leaflet model and constants file with categories

---
+++ server/src/models/leaflet.ts
@@ -0,0 +1,25 @@
+import { User } from "./user";
+import { leafletCategories as categories } from "../utils/constants/leaflet-categories";
+import { model, Schema } from "mongoose";
+
+export interface Leaflet {
+  user: User;
+  title: string;
+  description: string;
+  lookingFor: typeof categories.lookingFor[number];
+  levels: [typeof categories.levels[number]];
+  subjects: [typeof categories.subjects];
+}
+
+const leafletSchema = new Schema<Leaflet>({
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  lookingFor: { type: String, enum: categories.lookingFor, required: true },
+  levels: [{ type: String, enum: categories.levels, required: true }],
+  subjects: [{ type: String, enum: categories.subjects, required: true }],
+});
+
+const Model = model("Leaflet", leafletSchema);
+
+export default Model;
---
+++ server/src/utils/constants/leaflet-categories.ts
@@ -0,0 +1,15 @@
+export const leafletCategories = {
+  lookingFor: ["teacher", "students", "study partner"],
+  subjects: [
+    "Mathematics",
+    "English",
+    "Spanish",
+    "Polish",
+    "Physics",
+    "Computer Science",
+    "Biology",
+    "Chemistry",
+    "History",
+  ],
+  levels: ["Elementary school", "Middle school", "High school", "University"],
+};
