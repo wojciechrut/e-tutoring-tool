288fad0dfdfe42078c351ca71565a7d0cff2dc0e
wojciechrut <wojciechrut1998@gmail.com>
2022-10-28 19:57:01 +0200

    Typed scss modules config

--- client/next.config.js
+++ client/next.config.js
@@ -10,6 +10,16 @@ const nextConfig = {
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
+
+    config.module.rules
+      .find(({ oneOf }) => !!oneOf)
+      .oneOf.filter(({ use }) => JSON.stringify(use)?.includes("css-loader"))
+      .reduce((acc, { use }) => acc.concat(use), [])
+      .forEach(({ options }) => {
+        if (options.modules) {
+          options.modules.exportLocalsConvention = "camelCase";
+        }
+      });
     return config;
   },
 };
